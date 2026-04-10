56be94878a24f9007c8f48e383984e51b86b6257
TarikEren <tarikerentosun@outlook.com>
2026-04-11 02:30:43 +0300

    feat: added custom exceptions

--- src/pyagentkit/exceptions.py
+++ src/pyagentkit/exceptions.py
@@ -1,30 +1,75 @@
-class AgentExceptionError(Exception):
+# src/pyagentkit/exceptions.py
+
+
+class PyAgentKitError(Exception):
+    """Base exception for all pyagentkit errors"""
+
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ExceptionAgentError(PyAgentKitError):
     """Recoverable response exception"""
 
     def __init__(self, message: str):
-        self.message = message
-        super().__init__(self.message)
+        super().__init__(message)
 
 
-class AgentExceptionFatal(Exception):
+class ExceptionAgentFatal(PyAgentKitError):
     """Irrecoverable response exception"""
 
     def __init__(self, message: str):
-        self.message = message
-        super().__init__(self.message)
+        super().__init__(message)
 
 
-class ToolExceptionError(Exception):
+class ExceptionToolError(PyAgentKitError):
     """Recoverable tool calling exception"""
 
     def __init__(self, message: str):
-        self.message = message
-        super().__init__(self.message)
+        super().__init__(message)
 
 
-class ToolExceptionFatal(Exception):
+class ExceptionToolFatal(PyAgentKitError):
     """Irrecoverable tool exception"""
 
     def __init__(self, message: str):
-        self.message = message
-        super().__init__(self.message)
+        super().__init__(message)
+
+
+class ExceptionToolRetriesExhausted(PyAgentKitError):
+    """Raised when agent has exhausted its tool retries"""
+
+    def __init__(self, agent_name: str, retries: int):
+        self.agent_name = agent_name
+        self.retries = retries
+        super().__init__(
+            f"Agent {self.agent_name} has exhaused {self.retries} tool tries"
+        )
+
+
+class ExceptionResponseRetriesExhausted(PyAgentKitError):
+    """Raised when agent has exhausted its response retries"""
+
+    def __init__(self, agent_name: str, retries: int):
+        self.agent_name = agent_name
+        self.retries = retries
+        super().__init__(
+            f"Agent {self.agent_name} has exhaused {self.retries} response tries"
+        )
+
+
+class ExceptionEnvironmentError(PyAgentKitError):
+    """Raised if Ollama server is unreachable or model is not found"""
+
+
+class ExceptionInvalidTool(PyAgentKitError):
+    """Raised when tooling has missing or invalid fields"""
+
+
+class ExceptionFatalError(PyAgentKitError):
+    """Raised when fatal tool or agent error is caught"""
+
+
+class ExceptionUnhandledError(Exception):
+    """Raised for unhandled errors"""
